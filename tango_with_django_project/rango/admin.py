80cb0b808be45f2481c9cf279364922e35722b9a
Ethan <ethanscruton11@gmail.com>
2020-11-19 14:38:06 -0500

    Section 6

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from rango.models import Category, Page
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Import the Category and Page models
+from rango.models import Category, Page
+
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by no. likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary
+    # that will be passed to the template engine.
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
 
+def show_category(request, category_name_slug):
+    context_dict = {}
 
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context_dict)
