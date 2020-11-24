c6ca0514bffbd633979636903d46a97cf0014ae5
Ethan <ethanscruton11@gmail.com>
2020-11-23 19:38:37 -0500

    Section 8

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
 from django.urls import path
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
